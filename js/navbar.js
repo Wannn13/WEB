29e5aa1566f6dd88b7776949ba7e0755dc88d5d4
Wannn13 <muhammad.ridwan2552@gmail.com>
2022-03-24 11:20:29 +0700

    a

---
+++ js/navbar.js
@@ -0,0 +1,40 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top  = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+
+
+// NAVBAR 
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     window.addEventListener('scroll', function () {
+
+//         if (window.scrollY > 200) {
+//             document.getElementById('navbar_top').classList.add('fixed-top');
+//             // document.getElementById('navbar_top').classList.add('container');
+//             // document.getElementById('navbar_top').classList.add('container');
+//             // add padding top to show content behind navbar
+//             navbar_height = document.querySelector('.navbar').offsetHeight;
+//             document.body.style.paddingTop = navbar_height + 'px';
+//         } else {
+//             document.getElementById('navbar_top').classList.remove('fixed-top');
+//             // remove padding top from body
+//             document.body.style.paddingTop = '0';
+//         }
+//     });
+// });
+// DOMContentLoaded  end
